29c813e6d49955e66ccfc50fdd060b7d9952845d
sakhawatalir <jam752575@gmail.com>
2025-11-28 23:30:05 +0500

    Increase top padding on property detail page to add more space from navbar

--- src/app/[locale]/properties/[slug]/page.tsx
+++ src/app/[locale]/properties/[slug]/page.tsx
@@ -202,7 +202,7 @@ export default async function PropertyPage({ params: { locale, slug } }: Propert
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-900 via-blue-950 to-black">
       {/* Property Details - Show First */}
-      <div className="container mx-auto px-4 md:px-8 lg:px-12 xl:px-16 pt-20 pb-8">
+      <div className="container mx-auto px-4 md:px-8 lg:px-12 xl:px-16 pt-28 md:pt-32 pb-8">
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-12">
           {/* Main Content */}
           <div className="lg:col-span-2 space-y-12">
